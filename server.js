45517bc7aa326d5fd560144dc0b41c0eb0b65f24
Annie Clinton <annelisabethclinton1@gmail.com>
2024-05-15 11:07:55 -0700

    added || PORT to listen

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(express.json())
 app.use('/', homeRoutes)
 app.use('/todos', todoRoutes)
  
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
